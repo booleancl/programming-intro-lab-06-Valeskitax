a457ce8beab9b0211061404b9b75fc373acd51f9
val <ahumada.gonzalez.valeska@gmail.com>
2022-10-06 20:20:59 -0300

    funciones

---
+++ 01.functions.py
@@ -0,0 +1,21 @@
+
+def saludo ():
+    print ('Hola') 
+
+saludo () 
+
+
+def sum (a,b):
+    return a + b
+resultado =sum (1,2)
+print(resultado)    
+
+
+def resta (c=0,d=0):
+      return c - d
+print (resta ())
+print (resta (1))
+print (resta (100,1))
+
+
+
